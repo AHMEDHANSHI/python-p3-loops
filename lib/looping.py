bbe5259c1a1ac8158d2f5188d6aed87c09b50598
kamusarihanshi <ahmedizaka9@gmail.com>
2024-05-28 09:30:52 +0300

    labs

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   number = 11
+   while number>0 :
+       number=number-1
+       print(number)
+   print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    simon =[i*i for i in int_list]
+    return simon
 def fizzbuzz():
-    # code goes here!
-    pass
+   for i in range(1,101):
+       if i%3==0 and i%5==0:
+           print("FizzBuzz")
+       elif i%3==0:
+           print("Fizz")
+       elif i%5==0:
+           print("Buzz")
+       else:
+           print(i)
